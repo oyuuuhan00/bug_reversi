d27eb933bd944eed606a69aa7ff6e0432df19894
oyuuuhan <oyuuuhan@gmail.com>
2025-01-06 16:06:48 +0900

    全てのバグを解消しました

--- lib/reversi_methods.rb
+++ lib/reversi_methods.rb
@@ -47,7 +47,7 @@ module ReversiMethods
 
     # コピーした盤面にて石の配置を試みて、成功すれば反映する
     copied_board = Marshal.load(Marshal.dump(board))
-    copied_board[pos.col][pos.row] = stone_color
+    copied_board[pos.row][pos.col] = stone_color
 
     turn_succeed = false
     Position::DIRECTIONS.each do |direction|
@@ -61,14 +61,15 @@ module ReversiMethods
   end
 
   def turn(board, target_pos, attack_stone_color, direction)
+    return false if target_pos.stone_color(board) == BLANK_CELL
     return false if target_pos.out_of_board?
     return false if target_pos.stone_color(board) == attack_stone_color
-
+  
     next_pos = target_pos.next_position(direction)
-    if (next_pos.stone_color(board) == attack_stone_color) || turn(board, next_pos, attack_stone_color, direction)
+      if (next_pos.stone_color(board) == attack_stone_color) || turn(board, next_pos, attack_stone_color, direction)
       board[target_pos.row][target_pos.col] = attack_stone_color
       true
-    else
+      else
       false
     end
   end
@@ -81,11 +82,12 @@ module ReversiMethods
     board.each_with_index do |cols, row|
       cols.each_with_index do |cell, col|
         next unless cell == BLANK_CELL
-
+      
         position = Position.new(row, col)
         return true if put_stone(board, position.to_cell_ref, attack_stone_color, dry_run: true)
       end
     end
+    false
   end
 
   def count_stone(board, stone_color)
