c446dbd078c12fcb6f1ac5b0cbbf06cde1b0d88f
oyuuuhan <oyuuuhan@gmail.com>
2025-01-15 16:07:07 +0900

    1回目のレビューを修正しました

--- lib/reversi_methods.rb
+++ lib/reversi_methods.rb
@@ -64,7 +64,7 @@ module ReversiMethods
     return false if target_pos.stone_color(board) == BLANK_CELL
     return false if target_pos.out_of_board?
     return false if target_pos.stone_color(board) == attack_stone_color
-  
+
     next_pos = target_pos.next_position(direction)
       if (next_pos.stone_color(board) == attack_stone_color) || turn(board, next_pos, attack_stone_color, direction)
       board[target_pos.row][target_pos.col] = attack_stone_color
